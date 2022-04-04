376056be3dd3f05f53e196aef961d2970c49cbe3
Sungho <skyreon@gmail.com>
2022-04-04 23:09:32 +0900

    Update client_main.cpp

--- network/client/client_main.cpp
+++ network/client/client_main.cpp
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include "client_main.h"
+
 using namespace std;
 
 #define SOCKET_DATA_MAX 1024
@@ -21,17 +23,6 @@ using namespace std;
  * @brief Two-way communication 
  * 
  */
-class HelloNetClient
-{
-public:
-    int startSendMessageClient();
-    int startReceiveLoopback(char* ip_address);
-    int disconenct();
-
-private:
-    int net_connect_fd;
-};
-
 void *loop_receive(void *socket_fd)
 {
     cout << "Client Ready !!!" << endl;
